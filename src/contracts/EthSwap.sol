509bb7fa507c3118538ae118df642fa78deeb2e4
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2020-06-22 11:46:16 -0500

    Add buy token functionality and tests

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,9 +1,32 @@
 pragma solidity ^0.5.0;
+import './Token.sol';
 
 contract EthSwap {
 		string public name = 'EthSwap Instant Exchange';
+    Token public token;
+    uint public rate = 100;
+    
+    event TokenPurchased(
+      address account,
+      address token,
+      uint amount,
+      uint rate
+    );
 
-    constructor() public {
-      
+    constructor(Token _token) public {
+      token = _token;
+
+    }
+
+    function buyTokens() public payable {
+      uint tokenAmount = msg.value * rate;
+
+      require(token.balanceOf(address(this)) >= tokenAmount, 'There is enough balance in the exchange to excecute the purchase');
+
+      // Transfers tokens to the user
+      token.transfer(msg.sender, tokenAmount);
+
+      // Emit an event
+      emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
     }
 }
